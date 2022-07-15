8bdc5305ecc20f2ea6e228fb40485e11059ee5f9
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-15 20:59:10 +0200

    L

---
+++ Chapter_02/ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+System.Type type = typeof(int);
+Console.WriteLine(type);
+
+int i = 100;
+Console.WriteLine(i.GetType().FullName);
+Type tp = typeof(int);
+Console.WriteLine($"typeof {tp}");
+Console.WriteLine(typeof(String));
+Console.WriteLine(typeof(Double));
+Console.WriteLine(sizeof(sbyte));
+Console.WriteLine(int.MaxValue);
