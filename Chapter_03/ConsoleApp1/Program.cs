1537a25ec83df31ba0612776ef6f1bb78ee6e602
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-19 19:13:14 +0200

    19.07.2022

--- Chapter_03/ConsoleApp1/Program.cs
+++ Chapter_03/ConsoleApp1/Program.cs
@@ -7,3 +7,15 @@ WriteLine($"\"a\" is {a}, \"b\" is {b}");
 int c = 3;
 int d = ++c;
 WriteLine($"\"c\" is {c}, \"d\" is {d}");
+
+int e = 11;
+int f = 3;
+Console.WriteLine($"e is {e}, f is{f}");
+Console.WriteLine($"e+f={e+f}");
+Console.WriteLine($"e-f={e-f}");
+Console.WriteLine($"e*f={e*f}");
+Console.WriteLine($"e/f={e/f}");
+Console.WriteLine($"e%f={e % f}");
+double g = 11.0;
+WriteLine($"g is{g:N1},f is{f}");
+WriteLine($"g/f={g / f}");
