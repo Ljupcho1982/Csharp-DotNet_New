38d4925e864455f44ef7da215490e82b2452762b
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-19 21:09:22 +0200

    19.07.2022

---
+++ Chapter_03/BooleanOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+bool a = true;
+bool b = false;
+WriteLine($"AND  |a       |b       ");
+WriteLine($"a    |{a & a,-5}   |{a & b,-5}");
+WriteLine($"b    |{b & a,-5}   |{b & b,-5}");
+WriteLine();
+WriteLine($"OR   |a       |b   ");
+WriteLine($"a    |{a | a,-5}   |{a | b,-5} ");
+WriteLine($"b    |{b | b,-5}   |{b | b,-5}");
+WriteLine($"XOR  |a       |b   ");
+WriteLine($"a    |{a ^ a,-5}   |{a ^ b,-5} ");
+WriteLine($"b    |{b ^ a,-5}   |{b ^ b,-5} ");
+
+static bool DoStaff()
+{
+    WriteLine("I am doing some stuff!");
+    return true;
+};
+WriteLine();
+WriteLine($"a & DoStaff()={a & DoStaff()}");
+WriteLine($"b & DoStuff()={b & DoStaff()}");
+WriteLine();
+WriteLine($"a && DoStaff()={a && DoStaff()}");
+WriteLine($"b && DoStuff()={b && DoStaff()}");
---
+++ Chapter_03/SelectionStatements/Program.cs
@@ -0,0 +1,60 @@
+﻿using static System.Console;
+
+Console.BackgroundColor = ConsoleColor.Blue;
+
+string password = "ninja";
+if (password.Length < 8)
+{
+    WriteLine("Your password is to short use at least 8 characters");
+
+}
+else
+{
+    WriteLine("Your password is stong");
+
+};
+// add and remove the "" to change behavior
+
+object o = 3;
+int j = 4;
+if(o is int i)
+{
+
+    WriteLine($"{i}*{j}={i * j}");
+}
+else
+{
+
+    WriteLine("o is not int so o is can not multiply!");
+
+}
+
+int number = (new Random()).Next(1, 7);
+
+
+WriteLine($"My random number is {number}");
+
+switch (number)
+{
+
+    case 1:
+        WriteLine("one");
+        break;
+    case 2:
+        WriteLine("two");
+        goto case 1;
+    case 3://multiple case section
+        
+    case 4:
+        WriteLine("Three or four");
+        goto case 1;
+    case 5:
+        goto A_label;
+    default:
+        WriteLine("Default");
+        break;
+}
+
+WriteLine("After end of switch");
+A_label:
+WriteLine($"After A_label");
